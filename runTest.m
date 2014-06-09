30558a1dd6de46ce060f93a921f24e8d05adbb57
Eduardo L. Francisco <eduardolf85@gmail.com>
2014-06-08 23:00:53 -0300

    Improvement of treatment on RGB images.
    Signed-off-by: Eduardo L. Francisco <eduardolf85@gmail.com>

--- runTest.m
+++ runTest.m
@@ -18,29 +18,35 @@ function [idx img populations fitValues] = runTest(level)
 		otherwise
 			picture = level;
 	end
+	original = picture;
 	
-	if ~ isbool(picture)
+	if ~ isbool(picture)							# Transform to a Black & White image
 		m = median(median(median(picture)));
 		picture(find(picture >  m * 0.8)) = max(max(max(picture)));
 		picture(find(picture <= m * 0.8)) = min(min(min(picture)));
 	endif
 	
-	[numRows, numCols] = size(picture);		# Get the image size
+	[numRows, numCols, numZ] = size(picture);		# Get the image size
+	if numZ > 1
+		picture = picture(:, :, 1);					# Use only 1 color channel, converted into a B&W image
+		#picture = sum(picture, 3);
+	endif
 	sliceNumber = 0;
+	# Processe 255 rows at a time, cause we are passing only a pair of 8 bits per point (yPosition)
 	for rowNumber = 1:255:numRows
 		sliceNumber++;
 		sliceSize = rowNumber + 254;
 		if sliceSize > numRows
 			sliceSize = numRows;
 		endif
-		slice = picture(rowNumber:sliceSize, :, 1);
+		slice = picture(rowNumber:sliceSize, :);
 		disp(['rowNumber: ' num2str(rowNumber) "\t\tsliceSize: " num2str(sliceSize)]);
 		
 		## Setting variables "GA Options" to gaGnuOct
 		gaOptions.InitialPopulation = [];
 		gaOptions.MutationRate      = 0.25;
 		gaOptions.PopulationSize    = 5000;
-		gaOptions.EliteCount        = 3500;
+		gaOptions.EliteCount        = 3000;
 		gaOptions.Generations       = 20;
 		
 		[topIndividual pop popFitVals] = gaGnuOct(@(bits)fitGrid(slice, bits), 16, gaOptions);
@@ -62,7 +68,7 @@ function [idx img populations fitValues] = runTest(level)
 		%figure;
 	end
 	
-	#imshow(picture);
+	#figure; imshow(picture);
 	xPos      = [];
 	yFiltered = [];
 	
@@ -85,9 +91,12 @@ function [idx img populations fitValues] = runTest(level)
 		
 		#yFiltered = yPos(find(diff) <= diffMetric, :);
 		yFiltered = [ yFiltered; yPos(find(diff <= mediana * 1.10), :) ];
+		#yFiltered = [yFiltered; yPos];
 		
 		xPos = [ xPos; [ones(size(yFiltered)(1),1), repmat(numCols, size(yFiltered)(1),1)] ];
 	end
 	figure;
 	[idx img] = drawLine(picture, yFiltered, xPos);
+	figure;
+	[idx img] = drawLine(original, yFiltered, xPos);
 
